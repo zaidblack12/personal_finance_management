6a11d76071eb9fc11ff8ec7bba2188c11253b40c
Zaid <zaidblack12@gmail.com>
2024-11-01 03:14:46 +0530

    added post, delete, update api

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import psycopg2
 import json
+import time
 
 
 app = FastAPI()
@@ -25,8 +26,42 @@ def create_connection():
 def read_root(limit: int=100):
     conn = create_connection()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM transaction_data LIMIT %s", (limit,))
+    cur.execute("SELECT * FROM expense_tracker LIMIT %s", (limit,))
     rows = cur.fetchall()
     conn.close()
     return rows
 # End of root endpoint
+
+@app.post("/add_budget/")
+def add_budget(category: str, labels: str, amount: float, payee: str, note: str, payment_type: str, location: str):
+    current_datetime = time.strftime('%Y-%m-%d %H:%M:%S')
+    conn = create_connection()
+    
+    if conn is None:
+        raise HTTPException(status_code=500, detail="Database connection failed")
+
+    try:
+        cur = conn.cursor()
+        sql = """INSERT INTO expense_tracker (datetime, category, labels, amount, payee, note, payment_type, location)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+        val = (current_datetime, category, labels, amount, payee, note, payment_type, location)
+        cur.execute(sql, val)
+        conn.commit()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+    finally:
+        conn.close()
+    
+    return {"message": "Budget added successfully"}
+
+@app.delete("/truncate/")
+def truncate():
+    conn = create_connection()
+    cur = conn.cursor()
+    sql  = """TRUNCATE TABLE expense_tracker"""
+    cur.execute(sql)
+    conn.commit()
+    conn.close()
+    return {"message": "Table truncated successfully"}
+# End of API endpoints
+
